2de964ddfe7834d34968aa321f75d73a0c765730
James Hines <jameshines10@gmail.com>
2019-07-06 18:01:58 -0500

    Refactors the index function to set the initial value of the counter to the reduced values from the eventlog

--- src/index.js
+++ src/index.js
@@ -2,19 +2,20 @@ import './web-components/buttons-component';
 import './web-components/counter-component';
 import EventLog from './eventLog';
 import Storage from './storage'
-
-let eventLog;
+import reduce from "./reducer";
 
 window.addEventListener('load', () => {
     Storage.get().then( (events) => {
-        eventLog = new EventLog(events);
+        let eventLog = new EventLog(events);
+
+        let state = reduce(eventLog.events);
+
+        const main = document.querySelector('main');
+
+        const counterComponent = document.createElement('counter-component');
+        counterComponent.count = state.count;
+
+        main.appendChild(counterComponent);
+        main.appendChild(document.createElement('counter-buttons'));
     });
-
-    const main = document.querySelector('main');
-
-    const counterComponent = document.createElement('counter-component');
-    counterComponent.count = 0;
-
-    main.appendChild(counterComponent);
-    main.appendChild(document.createElement('counter-buttons'));
 });
